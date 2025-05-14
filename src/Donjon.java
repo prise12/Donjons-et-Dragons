95edfa18e7c22b5f684aeea963926cfcf574a17b
giorgipanazzolo <fabio.giorgi.panazzolo@gmail.com>
2025-05-14 12:06:37 +0200

    donjon qui marche pas encore

--- src/Donjon.java
+++ src/Donjon.java
@@ -3,40 +3,46 @@ import DED.Entite.Personnage;
 import java.io;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Donjon {
 
     LinkedHashMap<Entitee, Pair > m_dictEntitee;
     LinkedHashMap<Objet, Pair > m_dictObjet;
-    int[] m_tabLargeurDonjon ;
+
+    string[][] m_donjon;
+    int m_largeurMap;
 
 
     public void afficherDonjon(int nMap){
-        String fileName = "donjon"+ nMap +".txt";
+        writer.println(" A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W\n");
+        writer.println("*-----------------------------------------------------------------------*\n");
+        for(int i = 0; i<  m_largeurMap; i++ ){
+            Console.print(i+" ");
+            for(int j = 0; j<  m_largeurMap; j++ ) {
+                Console.print(m_donjon[i][j]);
 
-
-        Scanner obj = new Scanner(fileName);
-        while (obj.hasNextLine()){
-
-            Console.println(obj.nextLine());
+            }
         }
+    }
 
     }
 
-    public void genererDonjon(Entitee[] tabEntitee, Objet[] tabObjet, int nMap){
-        String fileName = "donjon"+ nMap +".txt";
-        PrintWriter writer = new PrintWriter(fileName, encoding);
-        writer.println(" A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W\n");
-        writer.println("*-----------------------------------------------------------------------* \n");
+    public void genererDonjon(){
 
-        for(int i = 0; i< m_tabLargeurDonjon[nMap]; i++ ){
+        for(int i = 0; i< m_largeurMap; i++ ){
+            for(int j = 0; j <  m_largeurMap; j++ ){;
+                m_donjon[i][j] = ".";
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry : m_dictEntitee.entrySet()) {
+
+        }
+
+        for(int i = 0; i< m_largeurMap; i++ ){
             writer.print(i+" ");
-            for(int j = 0; j< m_tabLargeurDonjon[nMap]; j++ ){
-                for(Map.entry<Entitee, Couple> entryEntitee : map.entrySet()){
-                    if(entryEntitee.getValue() == new Pair<>(i, j) ){
-                        writer.print(i+" "+ entryEntitee.getKey().getNom +" ");
-                    }
-                }
-
+            for(int j = 0; j<  m_largeurMap; j++ ){;
+                writer.println(m_donjon[i][j]);
             }
         }
 
