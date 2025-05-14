d0d2473c4a2fcd8eb17358da500bfc60c2cc5a65
giorgipanazzolo <fabio.giorgi.panazzolo@gmail.com>
2025-05-14 11:17:26 +0200

    donjon qui marche pas

--- src/Donjon.java
+++ src/Donjon.java
@@ -1,22 +1,45 @@
-import java.io.*;
+import DED.Entite.Personnage;
+
+import java.io;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 public class Donjon {
 
-    Objet[] m_tabObjet;
-    Entitee[] m_tabEntitee;
+    LinkedHashMap<Entitee, Pair > m_dictEntitee;
+    LinkedHashMap<Objet, Pair > m_dictObjet;
+    int[] m_tabLargeurDonjon ;
+
 
     public void afficherDonjon(int nMap){
+        String fileName = "donjon"+ nMap +".txt";
 
 
+        Scanner obj = new Scanner(fileName);
+        while (obj.hasNextLine()){
+
+            Console.println(obj.nextLine());
+        }
 
     }
 
-    public void genererDonjon(Entitee[] tabEntitee, Objet[] tabObjet){
-        String fileName = "my-file.txt";
-        String encoding = "UTF-8";
-
+    public void genererDonjon(Entitee[] tabEntitee, Objet[] tabObjet, int nMap){
+        String fileName = "donjon"+ nMap +".txt";
         PrintWriter writer = new PrintWriter(fileName, encoding);
-        writer.println("The first line");
-        writer.println("The second line");
+        writer.println(" A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W\n");
+        writer.println("*-----------------------------------------------------------------------* \n");
+
+        for(int i = 0; i< m_tabLargeurDonjon[nMap]; i++ ){
+            writer.print(i+" ");
+            for(int j = 0; j< m_tabLargeurDonjon[nMap]; j++ ){
+                for(Map.entry<Entitee, Couple> entryEntitee : map.entrySet()){
+                    if(entryEntitee.getValue() == new Pair<>(i, j) ){
+                        writer.print(i+" "+ entryEntitee.getKey().getNom +" ");
+                    }
+                }
+
+            }
+        }
+
         writer.close();
 
     }
