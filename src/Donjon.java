ca7ae3d7cef21200108423e8915e3f12b8ffd9bd
giorgipanazzolo <fabio.giorgi.panazzolo@gmail.com>
2025-05-20 11:56:38 +0200

    oui

--- src/Donjon.java
+++ src/Donjon.java
@@ -11,15 +11,15 @@ import java.util.Map;
 
 public class Donjon {
 
-    private LinkedHashMap<Entitee, int[] > m_dictEntitee;
+    private LinkedHashMap<Entite, int[] > m_dictEntite;
     private LinkedHashMap<Equipement, int[] > m_dictEquipement;
     private int[][] m_tabObstacles;
     private String[][] m_donjon;
     private int m_largeurMap;
     
-    public Donjon(LinkedHashMap<Entitee, int[] > dictEntitee, LinkedHashMap<Equipement, int[] > dictEquipement, int[][] tabObstacles, int largeurMap) {
+    public Donjon(LinkedHashMap<Entite, int[] > dictEntite, LinkedHashMap<Equipement, int[] > dictEquipement, int[][] tabObstacles, int largeurMap) {
         
-        this.m_dictEntitee = dictEntitee;
+        this.m_dictEntite = dictEntite;
         this.m_dictEquipement = dictEquipement;
         this.m_tabObstacles = tabObstacles;
         this.m_tabObstacles = tabObstacles;
@@ -31,16 +31,16 @@ public class Donjon {
 
     public void afficherDonjon(int nMap){
 
-        Console.println(" A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W\n");
-        Console.("*-----------------------------------------------------------------------*\n");
+        System.out.println(" A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W");
+        System.out.println("*-----------------------------------------------------------------------*");
         for(int i = 0; i<  m_largeurMap; i++ ){
 
-            Console.print(i+" ");
+            System.out.print(i+" ");
             for(int j = 0; j<  m_largeurMap; j++ ) {
-                Console.print(m_donjon[i][j]);
+                System.out.print(m_donjon[i][j]);
 
             }
-            Console.print("\n");
+            System.out.print("\n");
         }
     }
 
@@ -53,10 +53,10 @@ public class Donjon {
         }
 
         //On genere les creatures, objets, obstacles dans la map
-        for(Map.Entry<Entitee, Integer[]> entry : m_dictEntitee.entrySet()) {
-            this.m_donjon[entry.getvalue()[0]][entry.getvalue()[1]] = entry.getValue().getNom().substring(0,2);
+        for(Map.Entry<Entite, int[]> entry : m_dictEntite.entrySet()) {
+            this.m_donjon[entry.getValue()[0]][entry.getValue()[1]] = entry.getValue().getNom().substring(0,2);
         }
-        for(Map.Entry<Entitee, Integer[]> entry : m_dictObjet.entrySet()) {
+        for(Map.Entry<Equipement, Integer[]> entry : m_dictEquipement.entrySet()) {
             this.m_donjon[entry.getvalue()[0]][entry.getvalue()[1]] = entry.getValue().getNom().substring(0,2);
         }
         for(int[] coo : m_tabObstacles) {
@@ -64,20 +64,20 @@ public class Donjon {
         }
         
         for(int i = 0; i< m_largeurMap; i++ ){
-            writer.print(i+" ");
+            System.out.print(i+" ");
             for(int j = 0; j<  m_largeurMap; j++ ){;
-                writer.println(m_donjon[i][j]);
+                System.out.print(m_donjon[i][j]);
             }
         }
 
-        writer.close();
+        Console.close();
 
     }
 
-    public void addEntitee(Entitee creature, int[] coordonées) {
+    public void addEntitee(Entite entite, int[] coordonées) {
         if(coordonées[0] < this.m_largeurMap && coordonées[1] < this.m_largeurMap ) {
 
-            this.m_dictEntitee.Add(creature, coordonées)
+            this.m_dictEntite.Add(entite, coordonées);
         }
         else {
             throw new IllegalArgumentException("Les coordonées doivent êtres contenue dans la map.");
@@ -87,7 +87,7 @@ public class Donjon {
 
     public void addEquipement(Equipement equipement, int[] coordonées) {
         if(coordonées[0] < this.m_largeurMap && coordonées[1] < this.m_largeurMap ) {
-            this.m_dictEquipement.Add(equipement, coordonées);
+            this.m_dictEquipement.put(equipement, coordonées);
         }
         else {
             throw new IllegalArgumentException("Les coordonées doivent êtres contenue dans la map.");
