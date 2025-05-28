7ec1e7e47f6db2a61041e911ba277e5a5693e6f3
Bryan Kempf <bryan.kempf@etu.unistra.fr>
2025-05-28 10:24:10 +0200

    Modif Tour

--- src/ded/entite/Entite.java
+++ src/ded/entite/Entite.java
@@ -5,7 +5,7 @@ import ded.entite.race.Race;
 import ded.objet.Armure;
 
 public abstract class Entite {
-<<<<<<< HEAD
+
     private String m_nom;
     private int m_vie;
 
@@ -15,38 +15,23 @@ public abstract class Entite {
     private int m_dexterite ;
     private int m_vitesse;
     private int m_initiative;
-=======
-    protected String m_nom;
-    protected int m_vie;
-    protected int m_force;
-    protected int m_dexterite;
-    protected int m_vitesse;
-    protected int m_initiative;
 
     public class Entite (String nom) {
         this.m_nom = nom;
     }
->>>>>>> 0478158 (modif personnage et int)
 
-
-<<<<<<< HEAD
     public Entite(String nom){
         this.m_nom = nom;
     }
 
 
     //public abstract void attaquer(Entite entite);
-=======
-    public abstract void defense(int attaque);
->>>>>>> 0478158 (modif personnage et int)
 
-<<<<<<< HEAD
+    public abstract void defense(int attaque);
+
     public void defense(Integer attaque){
         this.m_vie -= attaque;
     }
-=======
-
->>>>>>> 65f6756 (modif personnage)
 
     public String getNom(){
         return this.m_nom;
